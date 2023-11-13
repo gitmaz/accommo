7af79c6e867359caef962bedd3a376e8225b0837
maziar navabi <mn.usyd@gmail.com>
2023-11-13 20:49:41 +1100

    compatible mock api endpoint for SydneyAreaSuburbsController

---
+++ backend/public/api-mock/sydney-area-suburbs/index.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+
+// Set CORS headers
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
+
+
+echo json_encode([
+    ["id"=>1, "name"=>"sub1"],
+    ["id"=>2, "name"=>"sub2"],
+    ["id"=>3, "name"=>"sub3"],
+    ["id"=>4, "name"=>"sub4"],
+]);
