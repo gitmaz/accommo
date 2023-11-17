9b040a8352ed704f53b3b74905eece0aa11ba8cf
maziar navabi <mn.usyd@gmail.com>
2023-11-17 15:55:38 +1100

    prepare for deployment

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,7 +3,8 @@
 header('Content-Type: application/json');
 
 // Set CORS headers
-header('Access-Control-Allow-Origin: http://localhost:8080');
+//header('Access-Control-Allow-Origin: http://localhost:8080'); //local
+header('Access-Control-Allow-Origin: http://54.79.206.109:8081'); //prd
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
 
