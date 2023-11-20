ed16c257300a538bce355815a4f93cd4a2f74e18
maziar navabi <mn.usyd@gmail.com>
2023-11-21 08:56:09 +1100

    change of public ip

--- backend/public/api-mock/areas/index.php
+++ backend/public/api-mock/areas/index.php
@@ -3,7 +3,8 @@ header('Content-Type: application/json');
 
 // Set CORS headers
 header('Access-Control-Allow-Origin: http://localhost:8080');
-$frontendOrigin = getenv('FRONTEND_ORIGIN');
+//$frontendOrigin = getenv('FRONTEND_ORIGIN'); //tbd
+$frontendOrigin = "52.63.57.194"; //to be removed after tbd above configured
 header("Access-Control-Allow-Origin: http://$frontendOrigin:8081"); //prd
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
